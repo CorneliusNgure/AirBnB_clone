92671dfb9250209084c84ed785c211c5706a2e3a
CorneliusNgure <kingconelius07@gmail.com>
2024-02-20 14:39:34 -0500

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -24,8 +24,8 @@ class BaseModel:
         """
 
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
         if kwargs:
             for i, j in kwargs.items():
@@ -42,7 +42,7 @@ class BaseModel:
         """
         Updates the 'updated_at' attribute to the current date and time.
         """
-        self.updated_at = datetime.utcnow()
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
